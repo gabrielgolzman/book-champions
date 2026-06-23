287ee1ad4a995efcda8fc8dc98b4c76fb9fdc95f
gabrielgolzman <gabrielgolzman@gmail.com>
2026-06-23 11:06:04 -0300

    [BE-2] Continue work

--- api/src/book/book.repository.ts
+++ api/src/book/book.repository.ts
@@ -1,6 +1,24 @@
 import { Repository } from "../shared/base.repository.js";
 import { Book } from "./book.js";
+import { BOOKS_MOCK } from "./book.mock.js";
 
 export class BookRepository implements Repository<Book> {
+
+    public findAll(): Book[] | undefined {
+        return BOOKS_MOCK;
+    }
+
+    public findOne(item: { id: string; }): Book | undefined {
+        return BOOKS_MOCK.find((book) => book.id === item.id);
+    }
+
+    public add(item: Book): Book | undefined {
+        BOOKS_MOCK.push(item);
+        return item;
+    }
+
+    public update(item: Book): Book | undefined {
+
+    }
 
 }
